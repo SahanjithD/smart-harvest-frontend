b75cd225f2151f0fb59b271d2657b17a9b41dc01
SahanjithD <ddsahanjith@gmail.com>
2025-07-07 18:23:30 +0530

    TaskList

--- smart-harverst-frontend/src/App.tsx
+++ smart-harverst-frontend/src/App.tsx
@@ -37,7 +37,7 @@ function App() {
               element={
                 <div className="min-h-screen bg-gray-50">
                   <Navbar />
-                  <main>
+                  <main className="pt-16">
                     <Outlet />
                   </main>
                 </div>
